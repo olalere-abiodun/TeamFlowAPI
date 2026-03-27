f17fb8366415952c1023dc2211597ee9451a9b0b
Olalere Sherifdeen Abiodun <shervie20@gmail.com>
2026-03-27 03:18:52 +0100

    fixed Organization crud

--- app/crud.py
+++ app/crud.py
@@ -15,15 +15,33 @@ def create_organization( organization: schema.createOrganization, db: Session, c
     db.refresh(db_organization)
     return db_organization
 
-# def get_organization_by_id(organization_id: int, db: Session = Depends(get_db),current_user: schema.CurrentUser = Depends(security.get_current_user)):
-#     db_organization = db.query(model.Organization).filter(model.Organization.id == organization_id ).first()
-#     if db_organization is None:
-#         raise HTTPException(status_code=404, detail="Organization not found")
+#List all organizations user belongs to
+def get_organizations_by_user(organization_id: int, db: Session, current_user: schema.CurrentUser):
+    db_organization = db.query(model.Organization).filter(model.Organization.owner_id == current_user.uid).all()
+    return db_organization
+    
+#Get organization details
+def get_organization(organization_id: int, db: Session, current_user: schema.CurrentUser):
+    db_organization = db.query(model.Organization).filter(model.Organization.id == organization_id).first()
+    return db_organization
 
-# def get_organizations(db: Session = Depends(get_db)):
-#     return db.query(model.Organization).all()
+# Update organization
+def update_organization(organization_id: int, db: Session, current_user: schema.CurrentUser, organization: schema.UpdateOrganization):
+    db_organization = model.Organization(
+        name = organization.name,
+        description = organization.description
+        )
+    db.add(db_organization)
+    db.commit()
+    db.refresh(db_organization)
+    return db_organization
 
-# def update_organization(organization_id: int, organization: schema.UpdateOrganization, db: Session = Depends(get_db)):
-#     db_organization = get_organization_by_id(organization_id, db)
-#     for key, value in organization.dict(exclude_unset=True).items():
-#         setattr(db_organization, key, value)
+#Delete organization
+def delete_organization(organization_id: int, db: Session, current_user: schema.CurrentUser):
+    db_organization = db.query(model.Organization).filter(model.Organization.id == organization_id).first()
+    if db_organization is None:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    db.delete(db_organization)
+    db.commit()
+
+
